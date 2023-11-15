819742bf1561c0fae8448c21f5325e179eee0545
vertocode <evertonvanoni1@gmail.com>
2023-11-15 20:14:26 -0300

    feat: home page

---
+++ components/Header/DefaultHeader.vue
@@ -0,0 +1,63 @@
+<template>
+  <header class="absolute inset-x-0 top-0 z-50">
+    <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div class="flex lg:flex-1">
+        <NuxtLink to="/" class="-m-1.5 p-1.5">
+          <img class="h-8 w-auto" src="https://i.imgur.com/E8WdocW.png" alt="logo" />
+        </NuxtLink>
+      </div>
+      <div class="flex lg:hidden">
+        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
+          <span class="sr-only">Open menu</span>
+          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+      <div class="hidden lg:flex lg:gap-x-12">
+        <NuxtLink v-for="item in navigation" :key="item.name" :to="item.href" class="text-sm font-semibold leading-6 text-gray-900">{{ item.name }}</NuxtLink>
+      </div>
+      <div class="hidden lg:flex lg:flex-1 lg:justify-end">
+        <NuxtLink to="/" class="text-sm font-semibold leading-6 text-gray-900">Docs <span aria-hidden="true">&rarr;</span></NuxtLink>
+      </div>
+    </nav>
+    <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
+      <div class="fixed inset-0 z-50" />
+      <DialogPanel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <div class="flex items-center justify-between">
+          <NuxtLink to="#" class="-m-1.5 p-1.5">
+            <span class="sr-only">Logo</span>
+            <img class="h-8 w-auto" src="https://i.imgur.com/E8WdocW.png" alt="logo" />
+          </NuxtLink>
+          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
+            <span class="sr-only">Close</span>
+            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="mt-6 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-6">
+              <NuxtLink v-for="item in navigation" :key="item.name" :to="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">{{ item.name }}</NuxtLink>
+            </div>
+            <div class="py-6">
+              <NuxtLink to="#" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Log in</NuxtLink>
+            </div>
+          </div>
+        </div>
+      </DialogPanel>
+    </Dialog>
+  </header>
+</template>
+
+<script setup>
+import {Dialog, DialogPanel} from "@headlessui/vue";
+import {Bars3Icon, XMarkIcon} from "@heroicons/vue/24/outline";
+import { ref } from 'vue'
+
+const navigation = [
+  { name: 'Get Started', href: '/' },
+  { name: 'Components', href: '/components' },
+  { name: 'Contribute', href: '/contribute' },
+  { name: 'Contact', href: '/contact' },
+]
+
+const mobileMenuOpen = ref(false)
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main-layout bg-green-200">
+    <DefaultHeader />
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import DefaultHeader from "~/components/Header/DefaultHeader.vue"
+</script>
+
+<style lang="scss">
+.main-layout {
+  min-height: 100vh;
+}
+</style>
